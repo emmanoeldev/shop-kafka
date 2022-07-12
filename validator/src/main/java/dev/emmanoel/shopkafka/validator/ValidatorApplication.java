594f9cc36bd784c0c33d98d9ddf379ba78e31384
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-07-12 15:47:34 -0700

    chore: rename package

--- validator/src/main/java/dev/emmanoel/shopkafka/validator/ValidatorApplication.java
+++ validator/src/main/java/dev/emmanoel/shopkafka/validator/ValidatorApplication.java
@@ -1,4 +1,4 @@
-package dev.emmanoel.shopkrafka.validator;
+package dev.emmanoel.shopkafka.validator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- validator/src/test/java/dev/emmanoel/shopkafka/validator/ValidatorApplicationTests.java
+++ validator/src/test/java/dev/emmanoel/shopkafka/validator/ValidatorApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.emmanoel.shopkrafka.validator;
+package dev.emmanoel.shopkafka.validator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
